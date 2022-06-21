bf1057e5a04f1b1e478a730b1d59bd5c12b49601
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-06-21 16:24:05 -0400

    started springboot project

---
+++ src/main/java/com/cognixia/jump/AnimalJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalJpaApplication.class, args);
+	}
+
+}
