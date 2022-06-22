2ac505c20cb97b502933176d5b4e010dacbc416e
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-06-22 16:30:45 -0400

    added put with json, get by family, post by json

--- src/main/java/com/cognixia/jump/controller/AnimalController.java
+++ src/main/java/com/cognixia/jump/controller/AnimalController.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +30,7 @@ public class AnimalController {
 				.body("Welcome to the api");
 	}
 	
-	@GetMapping("/animals")
+	@GetMapping("/animal")
 	public List<Animal> getStudents() {
 		return repo.findAll();
 	}
@@ -42,6 +45,20 @@ public class AnimalController {
 		return ResponseEntity.status(200)
 				.body(found.get());
 	}
+	
+	@GetMapping("/animal/family/{family}")
+	public ResponseEntity<?> getAnimalByFamily(@PathVariable String family) {
+		
+		List<Animal> found = repo.findByFamily(family);
+		
+		if(found.isEmpty()) {
+			return ResponseEntity.status(404).body("No listed animals are " + family + "s");
+		}
+		else {
+			return ResponseEntity.status(200).body(found);
+		}
+		
+	}
 	@DeleteMapping("/animal/{id}")
 	public ResponseEntity<?> deleteAnimal(@PathVariable int id) {
 		if(repo.existsById(id)) {
@@ -50,5 +67,35 @@ public class AnimalController {
 		}
 		return ResponseEntity.status(404).body("Animal Not Found.");
 	}
+	
+	
+	
+	@PostMapping("/animal")
+	public ResponseEntity<?> createAddress(@RequestBody Animal animal) {
+		
+		animal.setId(null);
+		
+		Animal created = repo.save(animal);
+		
+		return ResponseEntity.status(201).body(created);
+	}
+	@PutMapping("/animal")
+	public ResponseEntity<?> updateStudent(@RequestBody Animal animal) {
+		
+		boolean exists = repo.existsById(animal.getId());
+		
+		// can do update if id exists
+		if(exists) {
+		
+			Animal updated = repo.save(animal);
+			
+			return ResponseEntity.status(200).body(updated);
+			
+		}
+		else { // id doesn't exist, can't do update
+			
+			return ResponseEntity.status(404).body("Can't perform update, student doesn't exist");
+		}
+		
+	}
 }
-//vd
--- src/main/java/com/cognixia/jump/repository/AnimalRepository.java
+++ src/main/java/com/cognixia/jump/repository/AnimalRepository.java
@@ -1,5 +1,7 @@
 package com.cognixia.jump.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.cognixia.jump.model.Animal;
 
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal,Integer>{
-
+	public List<Animal> findByFamily(String family);
 }
